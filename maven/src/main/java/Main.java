94ac78a2c821c66e4526e11a8f45ffb96a312102
AndreyJustino <andreyjustino2020@gmail.com>
2024-12-05 17:19:36 -0300

    feat: inserindo dados no banco e separando conexão em metodos diferentes

--- maven/src/main/java/Main.java
+++ maven/src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Main {
@@ -10,10 +11,23 @@ public class Main {
 
         try{
             Connection connection = DriverManager.getConnection(url, user, password);
-
-            System.out.println(connection);
-
             System.out.println("Conectado com sucesso.");
+
+            String querySql = "INSERT INTO rocket_db.tbl_usuario (email, nome, idade) VALUES(?, ?, ?);";
+
+            String email = "cloe@mail.com";
+            String name = "Cloe";
+            int age = 4;
+
+            PreparedStatement pst = connection.prepareStatement(querySql);
+
+            pst.setString(1, email);
+            pst.setString(2, name);
+            pst.setInt(3, age);
+
+            pst.execute();
+            System.out.println("Dados inseridos com sucesso");
+
             connection.close();
         }catch (SQLException e){
             System.out.println("Erro ao conectar com o banco: " + e.getMessage());
